6fa7cf12f93844cdc83b6bbb8f70531b379eb5cd
Nicolas <neonicolas19@gmail.com>
2026-09-12 09:07:40 +0800

    Default to llama-3.1-8b-instant (llama-3.3-70b-versatile is enterprise-only on Groq)

--- config/services.php
+++ config/services.php
@@ -24,7 +24,7 @@ return [
 
     'groq' => [
         'key' => env('GROQ_API_KEY'),
-        'model' => env('GROQ_MODEL', 'llama-3.3-70b-versatile'),
+        'model' => env('GROQ_MODEL', 'llama-3.1-8b-instant'),
     ],
 
     'ses' => [
